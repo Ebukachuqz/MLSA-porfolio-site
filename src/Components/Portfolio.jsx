c82a8ae9bc291082e5105767ea6c6a036ffb92fb
Ebuka Chuqz <31506237+Ebukachuqz@users.noreply.github.com>
2024-02-24 01:57:27 +0000

    Commit made from VS Code Azure Static Web Apps

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/minato.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "Picture of MINATO NAMIKAZE";
 
 /**
  * Project list
@@ -31,28 +31,16 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "MERN Admin Panel",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "An Admin Panel that renders infographics of sales, revenues and client data, using tables, graphs, and a map.",
+    url: "https://admin-panel-client.onrender.com",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Recipe Blog",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
-  },
-  {
-    title: "My Resume Site",
-    description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "A blog web app enabling cooks to CRUD (create, read, update, delete) recipes, featuring user authentication, profile management, and a searchable platform for recipe exploration",
+    url: "https://recipe-blog-400f.onrender.com/",
   },
 ];
 
